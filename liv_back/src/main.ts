f9c70ecd6528bf1a53a3c37699168736df9c2391
GabrielRodrS <gsiqueira394@gmail.com>
2025-05-12 12:50:44 -0300

    Configurações2

--- liv_back/src/main.ts
+++ liv_back/src/main.ts
@@ -1,9 +1,23 @@
-import { NestFactory } from '@nestjs/core';
+/*import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({ origin: 'http://localhost:3001' });
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
+*/
+
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const frontendUrl = process.env.FRONTEND_URL || 'http://localhost:3001';
+  app.enableCors({ origin: frontendUrl });
+
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
